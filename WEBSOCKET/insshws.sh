c43888f9f8d7d8b33f47b8426aa52845caec4569
carntech <165342713+carntech@users.noreply.github.com>
2025-11-05 09:08:57 +0700

    Add files via upload

--- WEBSOCKET/insshws.sh
+++ WEBSOCKET/insshws.sh
@@ -1,38 +1,18 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/carntech/genom/main/"
-function setopserviswebsoket(){
-systemctl stop sshws.service
-tmux kill-session -t sshws
-systemctl stop ws-dropbear.service
-systemctl stop ws-stunnel.service
-systemctl stop ws-openssh.service
-systemctl stop ws-ovpn.service
-rm -f /etc/systemd/system/sshws.service
-rm -f /usr/bin/ssh-wsenabler
-rm -f /usr/local/bin/ws-dropbear
-rm -f /usr/local/bin/ws-stunnel
-rm -f /usr/local/bin/ws-openssh
-rm -f /usr/local/bin/ws-ovpn
-rm -f /etc/systemd/system/ws-dropbear.service
-rm -f /etc/systemd/system/ws-stunnel.service
-rm -f /etc/systemd/system/ws-openssh.service
-rm -f /etc/systemd/system/ws-ovpn.service
-rm -f /usr/bin/proxy3.js
-}
-function instalsshwebsocket(){
-#hapus pagar saat reinstall // uncomment cokk jancokk
-setopserviswebsoket
-cd /root
-wget -qO /usr/bin/ssh-wsenabler "${REPO}ssh-wsenabler"
-wget -qO /usr/bin/proxy3.js "${REPO}proxy3.js"
-wget -qO /usr/local/bin/ws-dropbear "${REPO}ws-dropbear"
-wget -qO /usr/local/bin/ws-stunnel "${REPO}ws-stunnel"
-wget -qO /usr/local/bin/ws-openssh "${REPO}ws-openssh"
-wget -qO /usr/local/bin/ws-ovpn "${REPO}ws-ovpn"
-wget -qO /etc/systemd/system/ws-ovpn.service "${REPO}ws-ovpn.service"
-wget -qO /etc/systemd/system/ws-dropbear.service "${REPO}ws-dropbear.service"
-wget -qO /etc/systemd/system/ws-stunnel.service "${REPO}ws-stunnel.service"
-wget -qO /etc/systemd/system/ws-openssh.service "${REPO}ws-openssh.service"
+# Proxy For Edukasi & Imclass
+
+
+
+wget -O /usr/local/bin/ssh-wsenabler https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ssh-wsenabler
+wget -O /usr/local/bin/proxy3.js https://raw.githubusercontent.com/carntech/genom/refs/heads/main/SYSTEM/proxy3.js
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-dropbear
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-stunnel
+wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-openssh
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-ovpn
+wget -O /usr/local/bin/ws-ovpn.service https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-ovpn.service
+wget -O /usr/local/bin/ws-dropbear.service https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-dropbear.service
+wget -O /usr/local/bin/ws-stunnel.service https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-stunnel.service
+wget -O /usr/local/bin/ws-openssh.service https://raw.githubusercontent.com/carntech/genom/refs/heads/main/WEBSOCKET/ws-openssh.service
 sleep 1
 chmod +x /usr/bin/ssh-wsenabler
 chmod +x /usr/bin/proxy3.js
@@ -43,20 +23,21 @@ chmod +x /etc/systemd/system/ws-dropbear.service
 chmod +x /etc/systemd/system/ws-stunnel.service
 chmod +x /etc/systemd/system/ws-openssh.service
 sleep 1
-systemctl daemon-reload
-systemctl enable ws-dropbear.service
-systemctl start ws-dropbear.service
-systemctl restart ws-dropbear.service
-systemctl enable ws-stunnel.service
-systemctl start ws-stunnel.service
-systemctl restart ws-stunnel.service
-systemctl enable ws-openssh.service
-systemctl start ws-openssh.service
-systemctl restart ws-openssh.service
-systemctl enable ws-ovpn.service
-systemctl start ws-ovpn.service
-systemctl restart ws-ovpn.service
-cat <<EOF > /etc/systemd/system/sshws.service
+sudo systemctl daemon-reload
+sudo systemctl enable ws-dropbear.service
+sudo systemctl start ws-dropbear.service
+sudo systemctl restart ws-dropbear.service
+sudo systemctl enable ws-stunnel.service
+sudo systemctl start ws-stunnel.service
+sudo systemctl restart ws-stunnel.service
+sudo systemctl enable ws-openssh.service
+sudo systemctl start ws-openssh.service
+sudo systemctl restart ws-openssh.service
+sudo systemctl enable ws-ovpn.service
+sudo systemctl start ws-ovpn.service
+sudo systemctl restart ws-ovpn.service
+
+cat <<EOF > /etc/systemd/system/ws-nontls.service
 [Unit]
 Description=WSenabler
 Documentation=By Carntech
@@ -71,22 +52,13 @@ RestartSec=1s
 [Install]
 WantedBy=multi-user.target
 EOF
-chmod +x /etc/systemd/system/sshws.service
-systemctl daemon-reload
-systemctl enable sshws.service
-systemctl start sshws.service
-systemctl restart sshws.service >/dev/null 2>&1
-service sshws restart
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-yellow "SSH WEBSOCKET TELAH AKTIF...!!"
-echo ""
-echo -e "$COLOR1 ${NC}  ${WH}[${COLOR1}INFO${WH}]${NC} ${COLOR1}•${NC} ${green}SSH Websocket Started${NC}"
-echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
-echo -e "$COLOR1 ${NC}                ${WH}• YADDY KAKKOII MAGELANG •${NC}                 $COLOR1 $NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+chmod +x /etc/systemd/system/ws-nontls.service
+sudo systemctl daemon-reload
+sudo systemctl enable ws-nontls.service
+sudo systemctl start ws-nontls.service
+sudo systemctl restart ws-nontls.service >/dev/null 2>&1
+sudo service ws-nontls.service restart
 
 #RESPONSE = 'HTTP/1.1 101 WebSocket <font color="lime"> Carntech </font><font color="yellow"> . </font><font color="red"> Cloudfront</font>\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 101 WebSocket <font color="lime"> Carntech </font><font color="yellow"> . </font><font color="red"> Cloudfront </font>\r\nContent-Length: 104857600000\r\n\r\n'
 
-}
-instalsshwebsocket
